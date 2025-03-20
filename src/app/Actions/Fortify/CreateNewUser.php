08b5c8a782ec4de4f2a8970fcd89aa94656baa9a
root <root@DESKTOP-OV6SCP4>
2025-03-20 20:29:22 +0900

    バリデーション関係

--- src/app/Actions/Fortify/CreateNewUser.php
+++ src/app/Actions/Fortify/CreateNewUser.php
@@ -3,10 +3,11 @@
 namespace App\Actions\Fortify;
 
 use App\Models\User;
+
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
-use Illuminate\Validation\Rule;
 use Laravel\Fortify\Contracts\CreatesNewUsers;
+use App\Http\Requests\RegisterRequest;
 
 class CreateNewUser implements CreatesNewUsers
 {
@@ -19,17 +20,14 @@ class CreateNewUser implements CreatesNewUsers
      */
     public function create(array $input): User
     {
-        Validator::make($input, [
-            'name' => ['required', 'string', 'max:255'],
-            'email' => [
-                'required',
-                'string',
-                'email',
-                'max:255',
-                Rule::unique(User::class),
-            ],
-            'password' => $this->passwordRules(),
-        ])->validate();
+
+        $registerRequestInstance = new RegisterRequest();
+
+        Validator::make(
+            $input,
+            $registerRequestInstance->rules(),
+            $registerRequestInstance->messages(),
+        )->validate();
 
         return User::create([
             'name' => $input['name'],
