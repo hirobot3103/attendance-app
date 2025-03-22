f0b9aa945732d1b2d6f567ed0f47a1fb672a9928
root <root@DESKTOP-OV6SCP4>
2025-03-22 22:47:02 +0900

    feature:マルチログイン、一部seeder

---
+++ src/database/migrations/2025_03_22_170621_create_attendances_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('attendances', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained('users');
+            $table->dateTime('clock_in')->nullable();  // 就業開始日時
+            $table->dateTime('clock_out')->nullable(); // 就業終了日時
+            $table->integer('status')->default(0);     // 0:デフォルト(就業前)、1:就業中 2:就業終了 3:休憩中 4:申請中 5:申請済
+            $table->timestamp('created_at')->useCurrent()->nullable();
+            $table->timestamp('updated_at')->useCurrent()->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('attendances');
+    }
+};
